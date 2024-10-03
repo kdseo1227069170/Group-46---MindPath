45bdcb9eac2e9316cec63bbe4da9599d97384c99
Sunny <gsidhu4@asu.edu>
2024-10-03 16:42:15 -0700

    intial commit

---
+++ reactpractice/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ reactpractice/src/components/MyInfo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+// function MyInfo() {
+//   return (
+//     <div>
+//       <h1> My name is Sunny Sidhu</h1>
+//       <p> I am a software developer</p>
+//       <ul>
+//         <li>Japan</li>
+//         <li>India</li>
+//         <li>Europe</li>
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default MyInfo;
+
+
