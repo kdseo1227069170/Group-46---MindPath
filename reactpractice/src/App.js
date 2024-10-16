82e5795298bf133beb76fc40fbefc39d20367860
Sunny <gsidhu4@asu.edu>
2024-10-16 14:39:04 -0700

    get more react practice

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -7,6 +7,11 @@ import ContactCard from './components/ContactCard';
 import todosData from './todosData';
 import React, {Component} from 'react';
 
+
+function handleMouseOver() {
+  console.log("I was clicked");
+}
+
 class App extends Component {
   // const date = new Date()
   // const hours =date.getHours()
@@ -35,18 +40,57 @@ class App extends Component {
 constructor () {
   super()
   this.state = {
-    name: "Sunny",
-    age:26
+    count:0
   }
+  this.handleClick = this.handleClick.bind(this)
+  this.handleMultiply = this.handleMultiply.bind(this)
+  this.handleDivisionByTwo = this.handleDivisionByTwo.bind(this)
+}
+handleClick() {
+  this.setState(prevState => {
+    return {
+      count : prevState.count + 1
+    }
+  })
+}
+handleMultiply() {
+  this.setState(prevState => {
+    return {
+      count :prevState.count * 2
+    }
+  })
+}
+handleDivisionByTwo() {
+  this.setState(prevState => {
+    return {
+      count : prevState.count /2
+    }
+  })
 }
 
   render() {
+    // let wordDisplay;
+    // if (this.state.isLoggedIn ===true) {
+    //   wordDisplay = "in"
+
+    // } else {
+    //   wordDisplay = "out"
+    // }
+    // const todoItems = this.state.todos.map(item=> <TodoItem key={item.id} item={item}/>)
     return (
       <div>
-      <h1>{this.state.name}</h1>
-      <h3>{this.state.age} years old</h3>
-
+        {/* <img onMouseOver={() => console.log("Hovered")} src="https://www.wwe.com/f/styles/talent_champion_lg/public/all/2024/03/The_Rock_PROFILE--927b15797eefad54a3bca4d2a15e4921.png" alt="The Rock" />
+        <br />
+        <br />
+        <button onMouseOver ={handleMouseOver}>Click Me</button> */}
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
+        <button onClick={this.handleMultiply}>Multiply By 2!</button>
+        <button onClick={this.handleDivisionByTwo}>Divide By 2!</button>
       </div>
+   
+    
+ 
     )
     }
   }
