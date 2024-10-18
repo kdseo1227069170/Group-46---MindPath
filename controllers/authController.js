3fd423471f8a6b5c019d7edf2863bebd03596028
Galin Todorov <gtodorov@asu.edu>
2024-10-17 22:37:30 -0700

    update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,3 +20,39 @@ exports.register = async (req, res) => {
 		if (existingUser) {
 			return res.status(400).json({ message: 'Email already exists'});
 		}
+		
+		// Create a new user
+		const newUser = new User({firstName, lastName, email, username, password, phoneNumber });
+		await newUser.save();
+		
+		return res.status(201).json({ message: 'User registered successfully' });
+	}catch (error) {
+		return res.status(500).json({ message: 'Server error', error });
+	}
+};
+
+// User login
+exports.login = async (req, res) => {
+	const { email, password, verificationCode } = req.body;
+	
+	try {
+		//find user by email
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(404).json({ message: 'User not found'});
+		}
+		
+		// Check password
+		const isMatch = await user.comparePassword(password);
+		if (!isMatch) {
+			return res.status(400).json({ message: 'Invalid password' });
+		}
+		
+		// Check for 2FA
+		if (user,is2FAEnabled) {
+			const twoFAEntry = await TwoFA.findOne({ userId: user._id });
+			
+			// Validate the provided 2FA code
+			if (!twoFAEntry || twoFAEntry.verificationCode !== verificationCode || moment().isAfter(twoFAEntry.expiresAt))	{
+				return res.status(400).json({ message: 'Invalid or expired 2FA code' });
+			}
