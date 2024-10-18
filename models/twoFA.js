2b94fe49e922739bd32cd9fcbf0da02a232b5e76
Galin Todorov <gtodorov@asu.edu>
2024-10-17 21:24:18 -0700

    create file twoFA.js and method for 2FA

---
+++ models/twoFA.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const TwoFASchema = new mongoose.Schema({
+	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+	verificationCode: { type: String, required: true },
+	expiresAt: { type: Date, required: true}
+});
+
+module.exports = mongoose.model('TwoFA, TwoFASchema);
