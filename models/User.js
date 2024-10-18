44e3d0b662adef70584dc61d3730d5dcad31f636
Galin Todorov <gtodorov@asu.edu>
2024-10-17 20:56:38 -0700

    addfields to UserSchema

--- models/User.js
+++ models/User.js
@@ -7,3 +7,12 @@ const UserSchema = new mongoose.Schema({
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
+	username: { type: String, required: true, unique: true },
+	password : { type: String, required: true },
+	phoneNumber: { type: String, required: true },
+	is2FAEnabled: { type: Boolean, default: false },
+	createdAt: { type: Date, default: Date, now},
+	lastLogin: { type: Date },
+});
+
+module.exports = mongoose.model('User', UserSchema);
