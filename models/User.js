a9207e7b7f776300dc040b5b21b77e8057fdc08f
Galin Todorov <gtodorov@asu.edu>
2024-10-17 21:17:09 -0700

    hash password method done in UserSchema

--- models/User.js
+++ models/User.js
@@ -22,5 +22,17 @@ UserSchema.pre('save', async function (next) {
 	if (!this.isModified('password')){
 		return next();
 	}
+	//Generate salt
+	const salt = await bcrypt.genSalt(10);
+	// Hash the password
+	this.password = await bcrypt.hash(this.password, salt);
+	next();
+});
 
+//Method to compare passwords
+UserSchema.methods.comparePassword = async function (password) {
+	return await bcrypt.compare(password, this.password);
+};
+
+//Export the user model
 module.exports = mongoose.model('User', UserSchema);
