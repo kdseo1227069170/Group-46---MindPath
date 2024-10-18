abc9b13d16a89de77f51efddc9dcd659e418938d
Galin Todorov <gtodorov@asu.edu>
2024-10-17 21:09:09 -0700

    hash password in UserSchema

--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@
 
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+// Define the User schema
 const UserSchema = new mongoose.Schema({
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
@@ -14,5 +16,11 @@ const UserSchema = new mongoose.Schema({
 	createdAt: { type: Date, default: Date, now},
 	lastLogin: { type: Date },
 });
+
+// Hash password before saving the user
+UserSchema.pre('save', async function (next) {
+	if (!this.isModified('password')){
+		return next();
+	}
 
 module.exports = mongoose.model('User', UserSchema);
