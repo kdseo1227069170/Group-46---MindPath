d3663455dc2d258b95a1c259f57fe4365b3877db
Galin Todorov <gtodorov@asu.edu>
2024-10-19 22:48:18 -0700

    update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = requre('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 
