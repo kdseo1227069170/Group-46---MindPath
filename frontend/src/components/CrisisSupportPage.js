f4a542791bf6514fecdadf6448d77187f57ad4b9
Robert Ibarra <robertibarra@MacBookPro.lan>
2024-10-28 19:07:47 -0700

    Created CrisisSupportPage

---
+++ frontend/src/components/CrisisSupportPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CrisisSupportPage = () => {
+    return (
+        <div style={styles.pageContainer}>
+            <h1>Get Help with Mental Health Services</h1>
+            <p>
+                If you are in crisis, there are services available to help.
+                This page offers information and resources for mental health support in Canada.
+            </p>
+
+            <section>
+                
+            </section>
+        </div>
+    );
+};
