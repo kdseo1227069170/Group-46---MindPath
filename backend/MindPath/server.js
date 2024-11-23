3c7783eacfc19a019a33ec7b2898c370c19fde53
Galin Todorov <gtodorov@asu.edu>
2024-11-22 23:42:38 -0800

    End Sprint 4

--- backend/MindPath/server.js
+++ backend/MindPath/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const authRoutes = require('./src/routes/auth');
+const authRoutes = require('./src/routes/authRoutes');
 const dashboardRoutes = require('./src/routes/admin'); // Use the correct path for admin dashboard routes
 require('dotenv').config();
 
