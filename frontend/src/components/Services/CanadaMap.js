273ee7185ad63776db07f55db49cc7b7f6e1aaff
Sunny <gsidhu4@asu.edu>
2025-02-01 15:06:11 -0800

    Still a work in progress US #178 Tasks #195 and Tasks #196. Added some styling changes to the interactive map. Still need to add services/resources to the map

--- frontend/src/components/Services/CanadaMap.js
+++ frontend/src/components/Services/CanadaMap.js
@@ -6,9 +6,12 @@ import {mapInteract} from './map-interact';
 const CanadaMap = () => {
     mapInteract();
     return (
-        <div className="canada-map">
-          <CanadaMapSVG width="900" height="900" />
-        </div>
+      <div className="canada-map">
+        <h1 style={{ textAlign: 'center' }}>Resources in each Province</h1>
+        <CanadaMapSVG width="900" height="900" />
+        <div id="cajstip"></div>
+
+      </div>
     );
 };
 
