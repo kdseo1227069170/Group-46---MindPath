cf366c78f810fb7bb0efa8c422e9394f711b2545
Robert Ibarra <robertibarra@Roberts-MacBook-Pro.local>
2025-02-02 11:40:42 -0800

    Updated Summary page

--- frontend/src/components/Summary.js
+++ frontend/src/components/Summary.js
@@ -1,21 +1,23 @@
 import React from "react";
 import './Summary.css';
 
+// Component for Summary page
 const Summary = () => {
     return (
         <div className="summary-container">
+            {/*Page title*/}
             <h2>My Mental Health Journey</h2>
-
+            {/*Past Appointments*/}
             <div className="summary-section">
                 <h3>Past Appointments</h3>
                 <p>Review your past sessions with here.</p>
             </div>
-
+            {/*Medication History*/}
             <div className="summary-section">
                 <h3>Medication History</h3>
                 <p>Track you prescribed medications here.</p>
             </div>
-
+            {/*Progress Notes*/}
             <div className="summary-section">
                 <h3>Progress Notes</h3>
                 <p>Monitor your improvement through recorded progress.</p>
